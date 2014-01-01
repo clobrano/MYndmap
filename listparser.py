370ae7ee9104c83701eb1c7b234142bdea07b21b
Carlo Lobrano <c.lobrano@gmail.com>
2014-01-01 10:44:20 +0100

    some attributes privatized

--- listparser.py
+++ listparser.py
@@ -8,11 +8,10 @@ from myndmapnode import MYndMapNode
 class ListParser(object):
 
     def __init__(self):
-        self.base_indent = 4  # Number of spaces representing and indentation
-        indent_pattern = '\s'
-        self.compiler = compile(indent_pattern)
-        self.markers = ['-','+','*']
-        self.node_cnt = 0
+        self._base_indent = 4  # Number of spaces representing and indentation
+        self._compiler = compile('\s')
+        self._markers = ['-','+','*']
+        self._node_cnt = 0
 
     def isMainTopic(self, line):
         if line.startswith('#'):
@@ -26,7 +25,7 @@ class ListParser(object):
         first_marker_pos = len(raw_note)
         note_marker = None
 
-        for marker in self.markers:
+        for marker in self._markers:
             pos = raw_note.find(marker)
 
             if -1 < pos < first_marker_pos:
@@ -35,17 +34,9 @@ class ListParser(object):
 
         if len(raw_note) != first_marker_pos and None != note_marker:
             split = raw_note.split(note_marker)
-            indent_lev = len(self.compiler.findall(split[0])) / self.base_indent
+            indent_lev = len(self._compiler.findall(split[0])) / self._base_indent
 
         return indent_lev
-
-    def purge_empty_lines(self, notes):
-        pass
-
-    def get_node_id(self, indent_lev):
-        id = '%d-%d' % (indent_lev, self.node_cnt)
-        self.node_cnt += 1
-        return id
 
     def parse(self, notes):
         assert type(notes) == list
@@ -58,6 +49,7 @@ class ListParser(object):
         self.myndmap.set_root(notes[0])
 
         for line in notes[1:]:
+            log.debug('line "%s" is empty? %s' % (line, line == ''))
             if line == '':
                 continue
             # get indentation level
